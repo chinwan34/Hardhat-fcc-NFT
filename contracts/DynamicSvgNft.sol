a8ef91d379f41a1de48443e5e828985856762e3a
Chin Wan <k21016106@kcl.ac.uk>
2023-03-23 19:15:00 +0000

    Finished first part of dynamic on-chain NFT contract

---
+++ contracts/DynamicSvgNft.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "base64-sol/base64.sol";
+
+contract DynamicSvgNft is ERC721 {
+    // Mint function
+    // Store SVG information
+    // Some logic to show which image (switch token URI)
+
+    uint256 private s_tokenCounter;
+    // Will convert SVG to Base64 using online resource
+    string private i_lowImageURI;
+    string private i_highImageURI;
+    string private constant base64EncodedSvgPrefix =
+        "data:image/svg+vml;base64,";
+
+    constructor(
+        string memory lowSvg,
+        string memory highSvg
+    ) ERC721("Dynamic SVG NFT", "DSN") {
+        s_tokenCounter = 0;
+    }
+
+    function svgToImageURI(
+        string memory svg
+    ) public pure returns (string memory) {
+        string memory svgBase64Encoded = Base64.encode(
+            bytes(string(abi.encodePacked(svg)))
+        );
+        return
+            string(abi.encodePacked(base64EncodedSvgPrefix, svgBase64Encoded));
+    }
+
+    // no need to pay money for it
+    function mintNft() public {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter = s_tokenCounter + 1;
+    }
+}
